0f21ce5f5592483122837d2782a70093a584fd24
KaungZM <kaung7192@gmail.com>
2020-11-14 03:45:16 +0630

    database in progress

--- lib/Todo_helper.dart
+++ lib/Todo_helper.dart
@@ -1,3 +1,44 @@
-class TodoHelper {
+import 'dart:async';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
+import 'Todo.dart';
+
+class TodoHelper {
+  static final _databaseName = 'todo.db';
+  static final _databaseVersion = 1;
+  static final table = 'todo';
+  TodoHelper._();
+  static final TodoHelper todoHelper = TodoHelper._();
+  static Database _database;
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database;
+    } else {
+      _database = await _initDatabase();
+      return _database;
+    }
+  }
+
+  _initDatabase() async {
+    String path = join(await getDatabasesPath(), _databaseName);
+    return await openDatabase(path,
+        version: _databaseVersion, onCreate: _onCreate);
+  }
+
+  _onCreate(Database db, int version) {
+    return db.execute(
+        'CREATE TABLE $table(id INTEGER PRIMARY KEY, task TEXT, done INTEGER)');
+  }
+
+  Future<void> insertTodo(Todo todo) async {
+    final Database db = await database;
+    await db.insert(table, todo.toMap());
+  }
+
+  Future<List<Map<String, dynamic>>> retrieveData() async {
+    final Database db = await database;
+    print(await db.query(table));
+    return await db.query(table);
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/Todo.dart';
 import 'package:flutter_todo/Todo_list.dart';
+import 'Todo_helper.dart';
 
 void main() {
   runApp(MyApp());
@@ -31,6 +32,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<Todo> list;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,7 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Container(
-        child: (list == null || list.length == 0) ? Center(child: Text("Add new")) : TodoList(list),
+        child: (list == null) ? Center(child: Text("Add new")) : TodoList(list),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: loadAddDialog,
@@ -47,36 +49,56 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  @override
+  void initState() {
+    super.initState();
+    TodoHelper.todoHelper
+        .retrieveData()
+        .then((value) => value.forEach((element) {
+              setState(() {
+                list = List()
+                  ..add(Todo(element['id'], element['task'],
+                      (element['done'] == 1) ? true : false));
+              });
+            }));
+  }
+
   var textController = TextEditingController();
 
   void loadAddDialog() {
-    showDialog(context: context,child: AlertDialog(
-      title: Text("Add new"),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextFormField(controller: textController),
-          RaisedButton(onPressed: addTodo, child: Text("Add"))
-        ],
-      ),
-    ));
+    showDialog(
+        context: context,
+        child: AlertDialog(
+          title: Text("Add new"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(controller: textController),
+              RaisedButton(onPressed: addTodo, child: Text("Add"))
+            ],
+          ),
+        ));
   }
 
   void addTodo() {
-    if(list == null) {
-      list = List()..add(Todo(1, textController.text, false));
+    if (list == null) {
+      Todo todo = Todo(1, textController.text, false);
+      list = List()..add(todo);
+      TodoHelper.todoHelper.insertTodo(todo);
       setState(() {
         _MyHomePageState();
         textController.clear();
         Navigator.of(context).pop();
       });
+    } else {
+      setState(() {
+        Todo todo = Todo(list.length++, textController.text, false);
+        this.list.add(todo);
+        TodoHelper.todoHelper.insertTodo(todo);
+        textController.clear();
+        Navigator.of(context).pop();
+      });
     }
-    else {
-    setState(() {
-      this.list.add(Todo(list.length++, textController.text, false));
-      textController.clear();
-      Navigator.of(context).pop();
-    });}
   }
 }
